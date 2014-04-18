79596c453a88bee53d5a4482c76076713c7f7239
Christian Wolf <christian.wolf@deliapps.com>
2014-04-18 18:08:19 -0400

    [+] - using express for simple routing

--- tard.js
+++ tard.js
@@ -1,5 +1,13 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World from Cloud9\n');
-}).listen(process.env.PORT);
+var express = require('express');
+
+var app = express();
+
+app.get('/races', function(req, res) {
+    res.send([{name:'demo race 1'}, {name:'demo race 2'}]);
+});
+app.get('/races/:id', function(req, res) {
+    res.send({id:req.params.id, name: "The Name", description: "description"});
+});
+
+app.listen(process.env.PORT);
+console.log('Listening on port '+process.env.PORT+'...');
